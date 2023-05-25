9dd1379758be5370202d888e740865fc50efabf0
Vladimir Zhulin <45514323+JesterVZ@users.noreply.github.com>
2023-05-25 17:17:24 +0500

    сгенерированы осноыные сущности

--- lib/domain/usecases/login/get_data_usecase.dart
+++ lib/domain/usecases/login/get_data_usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:megaohm_app/core/error/failure.dart';
 import 'package:megaohm_app/core/usecase/usecase.dart';
-import 'package:megaohm_app/data/entities/login/get_data_request.dart';
+import 'package:megaohm_app/data/entities/get_data/get_data_request.dart';
 import 'package:megaohm_app/domain/repositories/get_data/get_data_repository.dart';
 
 class GetdataUseCase extends UseCase<bool, GetDataRequest> {
